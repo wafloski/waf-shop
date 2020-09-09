9fe07c7c52540ec4c91d052915f0095ba9d2a0f3
mateusz.konopka <mateusz.konopka@g2a.com>
2020-09-09 21:08:44 +0200

    prisma and yoga config

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  // this is used for quick and simply get of data
+  items: forwardTo('db')
+
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
